6f18299f66bd548dd80f257dbbfefd4cb7cb2757
Nate Hayflick <nhayflick@gmail.com>
2015-03-24 23:48:23 -0700

    Add primitive slot animation

--- app/scripts/directives/thmb-slot-machine.js
+++ app/scripts/directives/thmb-slot-machine.js
@@ -14,6 +14,53 @@ angular.module('slotMachineApp')
       // require: '^ngModel',
       scope: {
         thmbSlots: '='
+      },
+      // TODO: check this
+      controller: function ($scope, $animate) {
+        // TODO: ensure persistence of results from spin to spin
+        var results;
+        var animations = [];
+
+        function play () {
+          results = [];
+          for (var i = 2; i < $scope.thmbSlots.length; i++) {
+            var result = Math.floor(Math.random() * 3) + 1
+            results.push(result);
+            spinSlot(i + 1, result);
+          }
+        }
+
+        function spinSlot (slot, result) {
+          // TODO: Attempt to shorten this
+          var base = 30 + result;
+          (function rotateOnce() {
+            console.log('rotate');
+            var promise = performAnimation(slot, base % 3 + 1);
+            promise.then( function() {
+              clearAnimation(slot, base % 3 + 1);
+              if (!base--) {return false};
+              rotateOnce();
+            });
+          })();
+        }
+
+        function performAnimation (slot, row) {
+          console.log('perform');
+          var querySelector = document.querySelector( 'thmb-slot-container:nth-child(' + slot + ') > thmb-slot-row:nth-child(' + row  + ')' );
+          var $el = angular.element(querySelector);
+          // $el.removeClass('slide-by');
+          // console.log($animate.addClass($el, 'slide-by'));
+          // Returns a promise that is resolved once the animation is finished
+          return $animate.addClass($el, 'slide-by');
+        }
+
+        function clearAnimation (slot, row) {
+          var querySelector = document.querySelector( 'thmb-slot-container:nth-child(' + slot + ') > thmb-slot-row:nth-child(' + row  + ')' );
+          var $el = angular.element(querySelector);
+          $el.removeClass('slide-by');
+        }
+
+        $scope.play = play;
       }
     };
   });
